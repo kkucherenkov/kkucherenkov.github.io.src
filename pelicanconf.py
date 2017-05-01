8781a0a889c8688bc5ed194310d9b05e3978b20d
Kirill Kucherenkov <k.kucherenkov@agilefusion.com>
2017-05-01 11:21:42 +0300

    peilcan src added

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Kirill Kucherenkov'
+SITENAME = 'Yet Another Developer Journal [YADJ]'
+SITEURL = 'kkucherenkov.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+FEED_ALL_RSS = 'rss/all.xml'
+CATEGORY_FEED_RSS = 'rss/%s.xml'
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 15
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
